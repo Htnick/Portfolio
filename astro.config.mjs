65d0aef7e57376ee5a7ee3d94f9b01c85e500a90
Htnick <heni1091@colorado.edu>
2026-08-29 14:25:55 -0600

    Fix Node version and update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Your GitHub Pages URL
-  site: 'https://<Htnick>.github.io',
-  // The repository name (omit base if your repo is named <your-username>.github.io)
-  base: '/<portfolio>/',
+  site: 'https://htnick.github.io',
+  base: '/Portfolio',
 });
